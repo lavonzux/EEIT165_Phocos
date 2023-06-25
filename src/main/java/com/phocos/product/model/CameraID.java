19beecd4c4e3da89d04671a8c99afb2729c778a8
lavonzux <lavonzux@gmail.com>
2023-06-26 01:23:14 +0800

    Update CameraID, LensID, CameraHome, LensHome

--- src/main/java/com/phocos/product/model/CameraID.java
+++ src/main/java/com/phocos/product/model/CameraID.java
@@ -6,15 +6,37 @@ import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 
+import jakarta.persistence.Query;
+
 public class CameraID implements IdentifierGenerator {
 
-    private int currentValue = 1;
+     private int currentValue = 1;
+        private boolean skipZero = true;
+
 
     @Override
     public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
         int generatedValue = currentValue;
-        currentValue += 2; // 基数递增
+
+        if (skipZero && generatedValue == 0) {
+            generatedValue = 1;
+        }
+        currentValue += 2; // 基數遞增
+
+        // 檢查生成的奇數是否已存在於數據庫中
+        while (isDuplicate(generatedValue, session)) {
+            generatedValue += 2; // 自動往下搜索
+        }
 
         return generatedValue;
+    }
+
+    private boolean isDuplicate(int value, SharedSessionContractImplementor session) {
+        // 執行查詢以檢查數據庫中是否存在相同的奇數值
+        Query query = session.createQuery("SELECT COUNT(c.productID) FROM Camera c WHERE c.productID = :value");
+        query.setParameter("value", value);
+        Long count = (Long) query.getSingleResult();
+
+        return count > 0;
     }
 }
--- src/main/java/com/phocos/product/model/LensID.java
+++ src/main/java/com/phocos/product/model/LensID.java
@@ -6,15 +6,37 @@ import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 
+import jakarta.persistence.Query;
+
 public class LensID implements IdentifierGenerator {
 
-    private int currentValue = 2; // 起始值为2，即第一个生成的值为2
+     private int currentValue = 2;
+        private boolean skipZero = true;
+
 
     @Override
     public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
         int generatedValue = currentValue;
-        currentValue += 2; // 偶数递增
+
+        if (skipZero && generatedValue == 0) {
+            generatedValue = 2;
+        }
+        currentValue += 2; 
+
+        // 檢查生成的奇數是否已存在於數據庫中
+        while (isDuplicate(generatedValue, session)) {
+            generatedValue += 2; // 自動往下搜索
+        }
 
         return generatedValue;
+    }
+
+    private boolean isDuplicate(int value, SharedSessionContractImplementor session) {
+
+        Query query = session.createQuery("SELECT COUNT(l.productID) FROM Lens l WHERE l.productID = :value");
+        query.setParameter("value", value);
+        Long count = (Long) query.getSingleResult();
+
+        return count > 0;
     }
 }
