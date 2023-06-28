cc864ff8a5619f3b23eb7d25320a123785a108d5
ipsan <a10912555@gmail.com>
2023-06-28 17:35:39 +0800

    Develop sendMail & Login Register feature
    add very much things for development

--- src/main/java/com/phocos/member/MemberRepository.java
+++ src/main/java/com/phocos/member/MemberRepository.java
@@ -5,5 +5,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Integer> {
-
+	
+	boolean existsByMemberAccount(String memberAccount);
 }
