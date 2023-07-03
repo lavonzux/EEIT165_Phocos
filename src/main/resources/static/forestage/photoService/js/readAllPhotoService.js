b820be5fd9aef67c1beb24b12b3a165cef19b9c8
lavonzux <lavonzux@gmail.com>
2023-07-03 20:03:52 +0800

    Updated prev/next, indicator of carousel
    prev/next, and indicators in carousel will only be visible when there're more then one ref pics

--- src/main/resources/static/forestage/photoService/js/readAllPhotoService.js
+++ src/main/resources/static/forestage/photoService/js/readAllPhotoService.js
@@ -11,10 +11,6 @@ const output = document.querySelector('.output');
 
 
 window.onload = function () {
-    // grabAllPhotoServices();
-    // grabRefPicImg(1003);
-
-    // grabRefPicIds(1030);
     grabAllCarousel();
 }
 
@@ -28,19 +24,14 @@ async function grabAllCarousel() {
 }
 
 
-
-
-function grabAllPhotoServices() {
-    const url = ContextPathname + '/photoService/api/ReadAll'
-    return axios.get(url)
-}
-
 function grabRefPicIds(serviceID) {
     const url = ContextPathname + '/referencePicture/api/getPicIds?serviceID=' + serviceID
     return axios.get(url)
         .then(res => {
-            let carouselhtml = carouselHtmlMaker(res.data, serviceID);
-            console.log(carouselhtml);
+            carouselHtmlMaker(res.data, serviceID);
+        })
+        .catch(err => {
+            console.log('Cannot find Reference Pictures for: ' + serviceID);
         })
 }
 
@@ -49,44 +40,52 @@ function carouselHtmlMaker(RefPicIds, serviceid) {
     const targetCarouselCard = document.querySelector(`[data-serviceid="${serviceid}"]`)
 
     let htmlStr = `
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div id="carousel-indicator-${serviceid}" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
             <div class="carousel-indicators">
     `
-    RefPicIds.forEach((pictureID, index) => {
-        if (index == 0) {
-            htmlStr += `
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="${index}" class="active" aria-current="true" aria-label="Slide 1"></button>
+
+    if (RefPicIds.length > 1) {
+
+        RefPicIds.forEach((pictureID, index) => {
+            if (index == 0) {
+                htmlStr += `
+                <button type="button" data-bs-target="#carousel-indicator-${serviceid}" data-bs-slide-to="${index}" class="active" aria-current="true" aria-label="Slide 1"></button>
             `
-        } else {
-            htmlStr += `
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="${index}" aria-label="Slide ${index + 1}"></button>
+            } else {
+                htmlStr += `
+                <button type="button" data-bs-target="#carousel-indicator-${serviceid}" data-bs-slide-to="${index}" aria-label="Slide ${index + 1}"></button>
             `
-        }
-    })
+            }
+        })
+    }
+
     htmlStr += `
             </div>
-        `
+    `
     RefPicIds.forEach((pictureID, index) => {
         let picImgElement = refPicImgHtmlMaker(pictureID)
         htmlStr += `
             <div class="p-1 carousel-item ${index == 0 ? 'active' : ''}">
                 ${picImgElement}
             </div>
-        `
-    });
-    htmlStr += `
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
     `
+    });
+
+    if (RefPicIds.length > 1) {
+        htmlStr += `
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carousel-indicator-${serviceid}" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carousel-indicator-${serviceid}" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        `
+    }
     targetCarouselCard.innerHTML = htmlStr
 }
 
