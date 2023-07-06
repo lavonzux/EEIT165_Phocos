ebdfa75c83706ece62ea431ad4300851992d220e
Wen <wen.yin.xxx@gmail.com>
2023-07-06 20:37:09 +0800

    forum all model update
    something new and something just join column.

--- src/main/java/com/phocos/forum/model/ArticleLikes.java
+++ src/main/java/com/phocos/forum/model/ArticleLikes.java
@@ -1,29 +1,31 @@
 package com.phocos.forum.model;
 
+import com.phocos.member.Member;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "articlePic")
-public class ArticlePic {
-
+@Table(name = "articleLikes")
+public class ArticleLikes {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer articlePicId;
+	private Integer articleLikeId;
 	
-	@Lob
-	private byte[] articlePic;
+	@ManyToOne
+	@JoinColumn(name="memberID")
+	private Member member;
 	
 	@ManyToOne
 	@JoinColumn(name = "articleId")
 	private Article article;
-}
 	
+	private Integer liked;
+}
--- src/main/java/com/phocos/forum/model/Comment.java
+++ src/main/java/com/phocos/forum/model/Comment.java
@@ -1,7 +1,52 @@
-//package com.taptac.forum.model;
-//
-//public class Comment {
-//
-//}
+ package com.phocos.forum.model;
 
+import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.phocos.member.Member;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "comment")
+public class Comment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer commentId;
+	private Integer parentCommentId;
+	private String commentContent;
+
+	@DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date commentPostTime;
+	
+	@DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date commentUpdateTime;
+	
+	private Integer commentLikes = 0;
+	
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name = "articleId")
+	private Article article;
+	
+	@JsonIgnore
+	@JsonBackReference
+	@ManyToOne
+	@JoinColumn(name = "memberID")
+	private Member member;
+}
