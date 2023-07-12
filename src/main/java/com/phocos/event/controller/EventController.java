07be3568556ba973e7f747dc9ec1bdf7a3b299b2
Akira12Hsu <131841609+Akira12Hsu@users.noreply.github.com>
2023-07-12 10:29:53 +0800

    Update EventController.java

--- src/main/java/com/phocos/event/controller/EventController.java
+++ src/main/java/com/phocos/event/controller/EventController.java
@@ -5,57 +5,67 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.phocos.event.model.Event;
 import com.phocos.event.model.EventService;
 
 
+
 @Controller
 public class EventController {
-	
 	@Autowired
-	private EventService eService;
+	private EventService eServ;
 
-	@GetMapping("/dashboard/event")
-	public String processMainAction(Model m) {
-		List<Event> events = eService.findAll();
-		m.addAttribute("events", events);
+	//取得所有資料
+	@GetMapping("/events")
+	public String findAll(Model model) {
+		List<Event> events = eServ.findAll();
+		
+		model.addAttribute("events",events);
 		return "backstage/event/Event";
 	}
 
-	@GetMapping("/dashboard/event/create")
+	//跳到新增頁面
+	@GetMapping("/event/insert")
 	public String insertPage() {
-		return "backstage/event/CreateEvent";
+		return "backstage/event/InsertEvent";
+	}
+	
+	//新增資料
+	@PostMapping("/event/insertData")
+	public String insert(@ModelAttribute("event") Event event) {
+		eServ.insertEvent(event);
+		return "redirect:/events";
 	}
 
-	@PostMapping("/dashboard/event/created")
-	public String createMember(@ModelAttribute("event") Event eve2) {
-		eService.insert(eve2);
-		return "redirect:/dashboard/event";
+	//刪除資料
+	@DeleteMapping("/event/delete")
+	public String deleteEvent(@RequestParam("eventID") Integer eventID) {
+		eServ.deleteByEventId(eventID);
+		return "redirect:/events";
 	}
-
-	@PostMapping("/dashboard/event/delete")
-	public void deleteEvent(@RequestParam("eventID") int eventID) {
-		eService.deleteById(eventID);
-	}
-
-	@PostMapping("/dashboard/event/update")
-	public String updateEvent1(@RequestParam("eventID") int eventID, Model m) {
-		Event event = eService.findById(eventID);
-		m.addAttribute("event", event);
+	
+	//修改資料
+	@GetMapping("/event/edit")
+	public String editPage(@RequestParam("eventID") Integer eventID, Model model) {
+		Event event = eServ.getById(eventID);
+		model.addAttribute("event", event);
 		return "backstage/event/UpdateEvent";
 	}
-
-	@PostMapping("/dashboard/event/updated")
-	public String updateEvent2(@RequestParam("eventID") int eventID,
-			@ModelAttribute("updateBean") Event updatedEvent) {
-		eService.updateEventById(eventID, updatedEvent);
-		return "redirect:/dashboard/event";
+	
+	@PutMapping("/event/editData")
+	public String editPost(@ModelAttribute(name="event") Event event) {
+		eServ.updateEventById(event.getEventID(), event.getMemberID(), 
+				event.getEventType(), event.getEventTopic(), event.getEventInfo(),
+				event.getEventDate());
+		
+		return "redirect:/events";
 	}
-
 
 }
