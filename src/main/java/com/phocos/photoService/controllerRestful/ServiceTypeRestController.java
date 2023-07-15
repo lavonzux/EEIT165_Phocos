9cf2c75c7613b937b3032d89ef3fe2af31109117
lavonzux <lavonzux@gmail.com>
2023-07-15 20:33:10 +0800

    Fix Pagination, show Deleted, PS info in ReadOne etc.
    Pagination in ReadAll fixed,
    Deleted PS will be shown in ReadAll,
    Popular types now shows with count
    complete info of a PS shown in ReadOne,

--- src/main/java/com/phocos/photoService/controllerRestful/ServiceTypeRestController.java
+++ src/main/java/com/phocos/photoService/controllerRestful/ServiceTypeRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.phocos.photoService.Dto.ServiceTypeAndCount;
 import com.phocos.photoService.model.ServiceType;
 import com.phocos.photoService.service.ServiceTypeService;
 
@@ -33,6 +34,15 @@ public class ServiceTypeRestController {
 	public List<ServiceType> popular5(@RequestParam(name = "topN", required = false) Integer topN) {
 		if(topN == null) topN = 6;
 		return sService.read_Nth_Popular(topN);
+	}
+	
+	
+	
+	
+	@GetMapping("/serviceType/api/popularAndCount")
+	public List<ServiceTypeAndCount> countAndPopular(@RequestParam(name = "topN", required = false) Integer topN) {
+		if(topN == null) topN = 5;
+		return sService.readNthPopularWithCount(topN);
 	}
 	
 }
